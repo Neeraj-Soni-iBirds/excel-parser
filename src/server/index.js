5d403580375b0b38e9e04ba6caf53c34ef5a5091
Neeraj <neerajsoni@ibirdsservices.com>
2020-02-21 12:46:44 +0530

    test

--- src/server/index.js
+++ src/server/index.js
@@ -1,60 +1,51 @@
 /* eslint-disable no-console */
 // eslint-disable-next-line no-undef
-const jsforce = require('jsforce');
-require('dotenv').config();
-const { SF_USERNAME, SF_PASSWORD, SF_TOKEN, SF_LOGIN_URL } = process.env;
-if (!(SF_USERNAME && SF_PASSWORD && SF_TOKEN && SF_LOGIN_URL)) {
-    console.error(
-        'Cannot start app: missing mandatory configuration. Check your .env file.'
-    );
-    process.exit(-1);
-}
-const conn = new jsforce.Connection({
-    loginUrl: SF_LOGIN_URL
-});
-conn.login(SF_USERNAME, SF_PASSWORD + SF_TOKEN, err => {
-    if (err) {
-        console.error(err);
-        process.exit(-1);
-    }
-});
+//const jsforce = require('jsforce');
+//require('dotenv').config();
+// const { SF_USERNAME, SF_PASSWORD, SF_TOKEN, SF_LOGIN_URL } = process.env;
+// if (!(SF_USERNAME && SF_PASSWORD && SF_TOKEN && SF_LOGIN_URL)) {
+//     console.error(
+//         'Cannot start app: missing mandatory configuration. Check your .env file.'
+//     );
+//     process.exit(-1);
+// }
+// const conn = new jsforce.Connection({
+//     loginUrl: SF_LOGIN_URL
+// });
+// conn.login(SF_USERNAME, SF_PASSWORD + SF_TOKEN, err => {
+//     if (err) {
+//         console.error(err);
+//         process.exit(-1);
+//     }
+// });
 module.exports = app => {
     app.get('/api/sessions', (req, res) => {
-        const soql = `SELECT Id, Name, toLabel(Room__c), Description__c, format(Date_and_Time__c) formattedDateTime,
-            (SELECT Speaker__r.Id, Speaker__r.Name, Speaker__r.Description, Speaker__r.Email, Speaker__r.Picture_URL__c FROM Session_Speakers__r)
-            FROM Session__c ORDER BY Date_and_Time__c LIMIT 100`;
-        conn.query(soql, (err, result) => {
-            if (err) {
-                res.sendStatus(500);
-            } else if (result.records.length === 0) {
-                res.status(404).send('Session not found.');
-            } else {
-                const formattedData = result.records.map(sessionRecord => {
-                    let speakers = [];
-                    if (sessionRecord.Session_Speakers__r) {
-                        speakers = sessionRecord.Session_Speakers__r.records.map(
-                            record => {
-                                return {
-                                    id: record.Speaker__r.Id,
-                                    name: record.Speaker__r.Name,
-                                    email: record.Speaker__r.Email,
-                                    bio: record.Speaker__r.Description,
-                                    pictureUrl: record.Speaker__r.Picture_URL__c
-                                };
-                            }
-                        );
+        var jsforce = require('jsforce');
+        var conn = new jsforce.Connection();
+        // eslint-disable-next-line consistent-return
+        conn.login(
+            'shree.r@gmail.com',
+            'ibirds12347pNh5h7EKJPKJPnQpYtK0Wr3a',
+            // eslint-disable-next-line consistent-return
+            function (err, res) {
+                console.log('test ' + JSON.stringify(res));
+                if (err) {
+                    return console.error(err);
+                }
+                // eslint-disable-next-line consistent-return
+                conn.query('SELECT Id, Name FROM Account', function (
+                    error,
+                    res2
+                ) {
+                    if (error) {
+                        return console.error(error);
                     }
-                    return {
-                        id: sessionRecord.Id,
-                        name: sessionRecord.Name,
-                        dateTime: sessionRecord.formattedDateTime,
-                        room: sessionRecord.Room__c,
-                        description: sessionRecord.Description__c,
-                        speakers
-                    };
+                    console.log(res2);
+                    response.json(res2);
                 });
-                res.send({ data: formattedData });
             }
+        ).catch(function (error) {
+            console.error(error);
         });
     });
 };
