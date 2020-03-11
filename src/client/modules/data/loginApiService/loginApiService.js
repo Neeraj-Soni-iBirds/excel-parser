228d0ca27cfef04048cfb0b192edaef820443f52
Neeraj <neerajsoni@ibirdsservices.com>
2020-03-11 15:30:28 +0530

    make it better

---
+++ src/client/modules/data/loginApiService/loginApiService.js
@@ -0,0 +1,18 @@
+const URL = "/api/create";
+let fields = [];
+export const performLogin = (metadata) => fetch(URL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: metadata
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('No response from server');
+        }
+        return response.json();
+    }).then(result => {
+        fields = result.data;
+        return fields;
+    });
