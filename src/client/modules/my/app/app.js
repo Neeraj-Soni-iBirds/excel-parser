2104c70a175e7eecc299c03f213fba85e183aeb2
Neeraj <neerajsoni@ibirdsservices.com>
2020-03-11 11:38:40 +0530

    make it better

--- src/client/modules/my/app/app.js
+++ src/client/modules/my/app/app.js
@@ -76,4 +76,7 @@ export default class App extends LightningElement {
             console.log("Object Created !! ", result);
         });
     }
+    openModal{
+        this.template.querySelector('#myModal').style.display = "block";;
+    }
 }
