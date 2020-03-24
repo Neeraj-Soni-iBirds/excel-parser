fdf4dc66ade78a453f20565f2d53015372313b0c
Neeraj <neerajsoni@ibirdsservices.com>
2020-03-24 15:07:26 +0530

    Updated Yarn lockfile

--- src/server/index.js
+++ src/server/index.js
@@ -76,7 +76,7 @@ module.exports = app => {
                     'Content-Type': 'application/json; charset=UTF-8',
                     'Accept': 'application/json'
                 },
-                body: {
+                body: { 
                     "state": "UploadComplete"
                 }
             };
