1979cc1f81ea70f79e24d967c76bb0b33f8977a2
Neeraj <neerajsoni@ibirdsservices.com>
2020-03-25 11:38:42 +0530

    Updated Yarn lockfile

--- src/server/index.js
+++ src/server/index.js
@@ -49,7 +49,7 @@ module.exports = app => {
                 "object": objectName,
                 "contentType": "CSV",
                 "operation": "insert",
-                "lineEnding": "CRLF"
+                "lineEnding": "LF"
             })
         };
         try {
